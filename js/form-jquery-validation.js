dd4edd02dd59b3f96a719dae180dce3531942a44
mouhameds2 <44154934+mouhameds2@users.noreply.github.com>
2018-11-13 16:14:33 +0100

    TP_3..

---
+++ js/form-jquery-validation.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+  $( function() {
+    $( "#datepicker" ).datepicker();
+  } );
+    $("#valider").click(function (event) {
+        event.preventDefault();
+        console.log("click");
+        var isError = false;
+        var divs = $('.champ');
+
+        divs.each(function () {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+            // do whatever
+            if ($(this).val().length== 0) {
+                $(".modal-title").html("message modal");
+                $(".modal-body").html("veuillez remplir tous les champs svp");
+                isError = true;
+                $('#myModal').modal("show");
+            }
+
+
+            else if  ($(this).val().length<5){
+                $("h5").html("message modal");
+                $("p").html("tous les champs doivent être supérieurs à 5 caractères");
+                isError = true;
+                $('#myModal').modal("show");
+                
+            }
+        });
+        if (isError == false) {
+            $(".modal-title").html("Bienvenu" + " "+$("#nom").val());
+            $(".modal-body").html("vous êtes né(e)s le" + "   "   +$("#datepicker").val() + "  "+"et vous habitez" );
+
+            $(".modal-body").append("<img src=' https://maps.googleapis.com/maps/api/staticmap?markers="+ $("#adresse").val() +"&zoom=10&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg '>");   
+        
+            $(".modal-footer").html(""+$("#adresse").val());
+            
+            $('#myModal').modal("show");
+        } else {
+            $('#myModal').modal("show");
+        }
+
+
+
+
+    });
+
+});
